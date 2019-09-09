0b633def1c5eb427f4a6c8ff4d8585c432799fc9
ScorpIan555 <ian.daley23@gmail.com>
2019-09-09 11:27:37 -0400

    fixed footer

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 const Footer = props => (
   <footer id="footer" style={props.timeout ? { display: 'none' } : {}}>
-    <p className="copyright">&copy;2019, Van Daley Industries, LLC Design: </p>
+    <p className="copyright">&copy;2019, Van Daley Industries, LLC </p>
   </footer>
 );
 
